479f312ec10605045d63923562d38291f85d9d3c
andy <andy@f254145c-2037-0410-9b08-ae12bbb4bf19>
2007-08-07 19:02:22 +0000

    Added

---
+++ andy/parse_test.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w
+#
+#  parse_test
+#
+#  Created by Andy Armstrong on 2006-11-25.
+#  Copyright (c) 2006 Hexten. All rights reserved.
+
+use strict;
+use lib qw(lib);
+use Geo::Gpx;
+use Data::Dumper;
+
+$| = 1;
+
+while (my $obj = shift) {
+    my $xml = undef;
+    
+    print "$obj\n";
+    
+    open(my $fr, '<', $obj) or die "Can't read $obj ($!)\n";
+    { local $/; $xml = <$fr>; }
+    close($fr);
+    
+    my $gpx = Geo::Gpx->new(xml => $xml); 
+    (my $not = $obj) =~ s/[.][^.]*$//;
+    $not .= '.xml';
+    open(my $fw, '>', $not) or die "Can't write $not ($!)\n";
+    print $fw $gpx->xml(), "\n";
+    close($fw);
+
+    system('xml_pp', '-i', $not);
+    open(my $dw, '>>', $not) or die "Can't write $not ($!)\n";
+    #my $dw = $fw;
+    print $dw "----------------------------------\n";
+    print $dw Dumper($gpx);
+    close($dw);
+}
---
+++ andy/t.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl -w
+#
+#  t
+#
+#  Created by Andy Armstrong on 2006-11-24.
+#  Copyright (c) 2006 Hexten. All rights reserved.
+
+use strict;
+use lib qw(lib);
+use Geo::Gpx;
+
+$| = 1;
+
+my $gpx = Geo::Gpx->new();
+
+print $gpx->_xml('test', 'pork');
+print $gpx->_xml('link', { href => 'http://hexten.net/', name => 'Frob' });
+print $gpx->_xml('wpt', { lat => 10, lon => 10, time => time() });
+print $gpx->_xml('count', [ {name=>'one'}, {name=>'two'}, {name=>'three'} ]);
+
+# 
+# my @wpt = (
+#     {
+#         # All standard GPX fields
+#         lat           => 54.786989,
+#         lon           => -2.344214,
+#         ele           => 512,
+#         time          => time(),
+#         magvar        => 0,
+#         geoidheight   => 0,
+#         name          => 'My house & home',
+#         cmt           => 'Where I live',
+#         desc          => '<<Chez moi>>',
+#         src           => 'Testing',
+#         link          => {
+#             href => 'http://hexten.net/',
+#             text => 'Hexten',
+#             type => 'Blah'
+#         },
+#         sym           => 'pin',
+#         type          => 'unknown',
+#         fix           => 'dgps',
+#         sat           => 3,
+#         hdop          => 10,
+#         vdop          => 10,
+#         pdop          => 10,
+#         ageofdgpsdata => 45,
+#         dgpsid        => 247
+#     },
+#     {
+#         # Fewer fields
+#         lat           => -38.870059,
+#         lon           => -151.210030,
+#         name          => 'Sydney, AU'
+#     }
+# );
+# 
+# $gpx->waypoints(\@wpt);
+# 
+# $gpx->name('Test');
+# $gpx->desc('Test data');
+# $gpx->author({ 
+#     name    => 'Andy Armstrong', 
+#     email   => {
+#         id      => 'andy', 
+#         domain  => 'hexten.net'
+#     },
+#     link    => { 
+#         href    => 'http://hexten.net/', 
+#         text    => 'Hexten' 
+#     } 
+# });
+# $gpx->copyright('(c) Anyone');
+# $gpx->link({
+#     href => 'http://www.topografix.com/GPX', 
+#     text => 'GPX Spec', 
+#     type => 'unknown' 
+# });
+# $gpx->time(time());
+# $gpx->keywords(['this', 'that', 'the other']);
+# 
+# print $gpx->xml();
